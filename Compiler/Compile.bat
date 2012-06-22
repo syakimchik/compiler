bc04fb0fc3712243f1d7a13e9bce9b95394ca7b9
Yakimchik Sergey <yakimchik7@gmail.com>
2012-06-22 02:26:31 +0200

    Done issue #17

--- Compiler/Compile.bat
+++ Compiler/Compile.bat
@@ -2,9 +2,18 @@
 MD Result
 
 java -jar Compiler.jar %1
+if not exist %~n1.j goto end
+
 java -jar lib/jasmin.jar %~n1.j
+if not exist %~n1.class goto end
 "c:\Program Files\Java\jdk1.7.0\bin\jar" cvfe %~n1.jar %~n1 %~n1.class
 
 del %~n1.class
 del %~n1.j
 move %~n1.jar Result
+
+CD Result
+
+@echo Start program:
+java -Dfile.encoding=Cp866 -jar %~n1.jar
+: end
